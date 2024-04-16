b82bfa76c48650c6f552d5b5d1ec5e3aea0265f0
Callum R. Renwick <cr330@student.le.ac.uk>
2024-04-16 17:31:27 +0100

    Add alternative method for Part 3b

--- Part3.py
+++ Part3.py
@@ -1,5 +1,6 @@
 from math import log
 from sys import argv
+import statistics
 
 from sklearn.cluster import AgglomerativeClustering
 
@@ -89,19 +90,65 @@ def align_from_clustering(cluster: AgglomerativeClustering, sequences: list):
     return sequences[-1]
 
 
-if __name__ == "__main__":
+def part3b_alternative():
+    """Alternative multiple alignment technique (not using AgglomerativeClustering)"""
     if len(argv) < 2:
         print("Usage: Part3.py <filename>")
-    else:
-        sequences: list = read_seqs(argv[1])
-        distance_matrix = compute_distances(sequences)
-        # print("Python distance matrix: {}".format(pretty_print_matrix(distance_matrix)))
-        cluster = make_cluster(distance_matrix)
-        final_alignment: list = align_from_clustering(cluster, sequences)
-        print("Final alignment: {}".format(final_alignment))
-        # Score alignment
-        m_align = MultipleAlignment()
-        print("Scoring multiple alignment...")
-        score: float = m_align.scoreMultipleAlignment(final_alignment)
-        print("Done.")
-        print("Score: {}".format(score))
+        return
+    sequences: list = read_seqs(argv[1])
+    align = MultipleAlignment()
+    distance_matrix = compute_distances(sequences)
+    
+    # Find the two sequences in distance_matrix with the smallest mutual distance
+    minimum_dist = distance_matrix[0][1]
+    minimum_dist_index = (0, 1)
+    for row in distance_matrix:
+        diagonal_index = 0
+        for item in row:
+            if row.index(item) == diagonal_index:
+                continue
+            if item < minimum_dist:
+                minimum_dist = item
+                minimum_dist_index = (distance_matrix.index(row), row.index(item))
+        diagonal_index += 1
+    
+    # Having found sequences with minimum mutual distance, align those sequences first
+    profile = align.computeMultipleAlignment((sequences[minimum_dist_index[0]],
+        sequences[minimum_dist_index[1]]))
+    
+    aligned_indices = [i for i in minimum_dist_index]
+    # Repeat until all the sequences have been aligned
+    while len(aligned_indices) < len(sequences):
+        # Find the average distance of each sequence from the sequences already aligned
+        # Make a virtual row based on the average of rows for sequences in the alignment
+        average_distances = [statistics.mean([row[col] for row in distance_matrix])
+                for col in range(len(distance_matrix))]
+        # Choose the sequence from this row which has the lowest distance but isn't already in the alignment
+        minimum_new_distance = min(filter(lambda el: average_distances.index(el) not in aligned_indices,
+            average_distances))
+        # We are only interested in which sequence this distance corresponds to
+        minimum_new_distance_index = average_distances.index(minimum_new_distance)
+        # Add the new nearest sequence to the alignment
+        profile = align.computeMultipleAlignment((profile, sequences[minimum_new_distance_index]))
+        # Update aligned_indices with the index of the newly-aligned sequence
+        aligned_indices.append(minimum_new_distance_index)
+
+    return profile
+
+if __name__ == "__main__":
+    # if len(argv) < 2:
+    #     print("Usage: Part3.py <filename>")
+    # else:
+    #     sequences: list = read_seqs(argv[1])
+    #     distance_matrix = compute_distances(sequences)
+    #     # print("Python distance matrix: {}".format(pretty_print_matrix(distance_matrix)))
+    #     cluster = make_cluster(distance_matrix)
+    #     final_alignment: list = align_from_clustering(cluster, sequences)
+    #     print("Final alignment: {}".format(final_alignment))
+    #     # Score alignment
+    #     m_align = MultipleAlignment()
+    #     print("Scoring multiple alignment...")
+    #     score: float = m_align.scoreMultipleAlignment(final_alignment)
+    #     print("Done.")
+    #     print("Score: {}".format(score))
+    print("Profile: {}".format(part3b_alternative()))
